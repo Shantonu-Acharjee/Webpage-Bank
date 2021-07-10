16fe45ca591fefc453efd9f737286df1bd0170c5
Shantonu Acharjee <shantonuacharjee@gmail.com>
2021-07-10 00:33:05 -0700

    basic templet done

--- COMPLETE WEBSITE BANK/E-COMMERCE WEBSITE/1.Make Ecommerce Website Using HTML And CSS Step By Step  Create e-Commerce Website/js/main.js
+++ COMPLETE WEBSITE BANK/E-COMMERCE WEBSITE/1.Make Ecommerce Website Using HTML And CSS Step By Step  Create e-Commerce Website/js/main.js
@@ -27,31 +27,7 @@ function menutoggle(){
 
 
 
-/*----------------------------------------
-        <!-- JS FOR PRODUCT DETAILS START  -->------------------------------------------------------
-----------------------------------------*/
-var productImg = document.getElementById('productImg');
-var smallImg = document.getElementsByClassName('small-img');
-
-smallImg[0].onclick = function(){
-    productImg.src = smallImg[0].src;
-};
 
 
-smallImg[1].onclick = function(){
-    productImg.src = smallImg[1].src;
-};
 
 
-smallImg[2].onclick = function(){
-    productImg.src = smallImg[2].src;
-};
-
-
-smallImg[3].onclick = function(){
-    productImg.src = smallImg[3].src;
-};
-
-/*----------------------------------------
-        <!-- JS FOR PRODUCT DETAILS START  -->------------------------------------------------------
-----------------------------------------*/
